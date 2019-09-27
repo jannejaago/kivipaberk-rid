b7e337cd71916da0b7c085a92677785fe490e8cd
Opilane TTHK <opilane@tthk.ee>
2019-09-27 18:11:46 +0300

    modifications

--- kivipaberkäärid/Program.cs
+++ kivipaberkäärid/Program.cs
@@ -6,17 +6,27 @@ namespace kivipaberkäärid
     {
         static void Main(string[] args)
         {
+            string kivi = 1;
+            string käärid = 2;
+            string paber = 3;
+            string userRoll; 
+
             Console.WriteLine("Hello World! Lets play kivi paber käärid!");
-            value = Console.ReadLine();
-            Console.WriteLine($"Your weapon is {value}.");
+            userRoll = Console.ReadLine();
+            Console.WriteLine($"Your weapon is {userRoll}.");
+
+            if (userRoll == kivi)
+            {
+                kivi = 1;
+            }
 
             Random randomGenerator = new Random();
             int randomNumber = randomGenerator.Next(1, 4);
 
             if (randomNumber == 1) 
-                {
+            {
                 Console.WriteLine("My weapon is Kivi");
-                }
+            }
 
             else if (randomNumber == 2)
             {
