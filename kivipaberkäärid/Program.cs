e696a6e51ae0b75d6d537be34ae150f39bbc038e
kebo <bogdan.parubok@hotmail.com>
2019-09-30 19:41:51 +0300

    Case/default/break works now, small bug written inside code comment. Else just works

--- kivipaberkäärid/Program.cs
+++ kivipaberkäärid/Program.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 
+
+/* BUGS!!! 
+ * As the third point is given to either player, game still asks for user for its weapon which it shouldn't. Game should just finish with score table
+ * without asking for user weapon again.
+ */
+
 namespace kivipaberkäärid
 {
     class Program
@@ -7,9 +13,8 @@ namespace kivipaberkäärid
         static void Main()
         {
             int userRoll;
-            int cpuPoint = 0;
-            int userPoint = 0;
-            //int rolls = 0;
+            int cpuScore = 0;
+            int userScore = 0;
             string name;
 
             //Introduction
@@ -18,95 +23,111 @@ namespace kivipaberkäärid
             name = Console.ReadLine();
             Console.WriteLine();
             Console.WriteLine($"Hello {name}. Lets begin?");
-            Console.Write("Game rules:");
+            Console.Write("Game rules: ");
             Console.WriteLine("Paper bears Rock, Rock beats Scissors, Scissors beat Paper.");
             Console.WriteLine("Scissors corresponds to number 1, Rock corresponds to number 2, Paper correspond to number 3.");
             Console.WriteLine("Game goes on until one of us reaches 3 points. Winner takes all!");
             Console.WriteLine("Lets begin! Insert your weapon (number).");
-            userRoll = int.Parse(Console.ReadLine());
-           
-            //Checking for incorrect User input and declaration of user number input into plain simple words (User choosing weapon). 
-            switch (userRoll)
-            {
-                case 1:
-                    Console.WriteLine();
-                    Console.WriteLine("Your weapon is Scissors.");
-                    break;
-                case 2:
-                    Console.WriteLine();
-                    Console.WriteLine("Your weapon is Rock.");
-                    break;
-                case 3:
-                    Console.WriteLine();
-                    Console.WriteLine("Your weapon is Paper.");
-                    break;
-                default:
-                    Console.WriteLine();
-                    Console.WriteLine("Please, insert correct value, either of 1 or 2 or 3, which correspond to appropriate weapon.");
-                    break;
-            }
 
-            //Random number generator for CPU, scaled down to only 3 random numbers - 1 or 2 or 3.
-            if (userRoll >= 1 && userRoll <= 3)
+            //Playing until either one gets 3 points.
+            while (userScore < 3 && cpuScore < 3)
             {
-                //CPU choosing its weapon
-                Random randomGenerator = new Random();
-                int randomNumber = randomGenerator.Next(1, 4);
+                //User weapon choise input.
+                userRoll = int.Parse(Console.ReadLine());
 
-                if (randomNumber == 1)
+                //Checking for incorrect User input and declaration of User number input into plain simple words (User choosing weapon). 
+                switch (userRoll)
                 {
-                    Console.WriteLine("My weapon is Scissors.");
+                    case 1:
+                        Console.WriteLine();
+                        Console.WriteLine("Your chosen weapon is Scissors.");
+                        break;
+                    case 2:
+                        Console.WriteLine();
+                        Console.WriteLine("Your chosen weapon is Rock.");
+                        break;
+                    case 3:
+                        Console.WriteLine();
+                        Console.WriteLine("Your chosen weapon is Paper.");
+                        break;
+                    default:    
+                        Console.WriteLine();
+                        Console.WriteLine("Please, insert correct value, either of 1 or 2 or 3, which correspond to appropriate weapon.");
+                        break;
+                }
+                
+                //If user inserted correct number, game goes on.
+                if (userRoll >= 1 && userRoll <= 3)
+                {
+                    //CPU choosing its weapon, random number generator for CPU, scaled down to only 3 random numbers - 1 or 2 or 3.
+                    Random randomGenerator = new Random();
+                    int randomNumber = randomGenerator.Next(1, 4);
+
+                    if (randomNumber == 1)
+                    {
+                        Console.WriteLine("My weapon is Scissors.");
+                    }
+
+                    else if (randomNumber == 2)
+                    {
+                        Console.WriteLine("My weapon is Rock.");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("My weapon is Paper.");
+                    }
+
+                    //Deciding who is the winner.
+                    if ((randomNumber == 3 && userRoll == 2) || (randomNumber == 2 && userRoll == 1) || (randomNumber == 1 && userRoll == 3))
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("You LOOSE.");
+                        cpuScore++;
+                        Console.WriteLine($"I have now {cpuScore} points.");
+                        Console.WriteLine("Choose your weapon again.");
+                        Console.WriteLine();
+                    }
+
+                    else if ((randomNumber == 2 && userRoll == 3) || (randomNumber == 1 && userRoll == 2) || (randomNumber == 3 && userRoll == 1))
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("You WIN.");
+                        userScore++;
+                        Console.WriteLine($"You have now {userScore} points.");
+                        Console.WriteLine("Choose your weapon again.");
+                        Console.WriteLine();
+                    }
+
+                    else if ((randomNumber == 1 && userRoll == 1) || (randomNumber == 2 && userRoll == 2) || (randomNumber == 3 && userRoll == 3))
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("It is a TIE, none of us gets point.");
+                        Console.WriteLine("Choose your weapon again.");
+                        Console.WriteLine();
+                    }                                      
                 }
 
-                else if (randomNumber == 2)
-                {
-                    Console.WriteLine("My weapon is Rock.");
-                }
-
+                //Game continues until either of players get 3 points.
                 else
                 {
-                    Console.WriteLine("My weapon is Paper.");
-                }
-
-                //Deciding who is the winner.
-                if ((randomNumber == 3 && userRoll == 2) || (randomNumber == 2 && userRoll == 1) || (randomNumber == 1 && userRoll == 3))
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("You LOOSE.");
-                    cpuPoint++;
-                    Console.WriteLine($"I have now {cpuPoint} points.");
-                    //Console.WriteLine("Choose your weapon again.");
-                    Console.WriteLine();
-                }
-
-                else if ((randomNumber == 2 && userRoll == 3) || (randomNumber == 1 && userRoll == 2) || (randomNumber == 3 && userRoll == 1))
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("You WIN.");
-                    userPoint++;
-                    Console.WriteLine($"You have now {userPoint} points.");
-                    //Console.WriteLine("Choose your weapon again.");
-                    Console.WriteLine();
-                }
-
-                else
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("It is a TIE.");
-                    //Console.WriteLine("Choose your weapon again.");
-                    Console.WriteLine();
+                    continue;
                 }
             }
-
-            /*if (cpuPoint == 3)
+            if (cpuScore == 3)
             {
-                Console.WriteLine($"I have now {cpuPoint} and you have now {userPoint}. That means I won.");
+                Console.WriteLine();
+                Console.WriteLine("One of us won the game. Lets see who it was based on our game score.");               
+                Console.WriteLine($"I have now {cpuScore} and You have now {userScore}. That means I won.");
             }
 
-            else if (userPoint == 3)
+            else if (userScore == 3)
             {
-                Console.WriteLine($"You have now {userPoint} and I have now {cpuPoint}. That means you won.");
-            }*/
+                Console.WriteLine();
+                Console.WriteLine("One of us won the game. Lets see who it was based on our game score."); 
+                Console.WriteLine($"You have now {userScore} and I have now {cpuScore}. That means You won.");
+            }
+            Console.ReadLine();
         }
     }
 }
